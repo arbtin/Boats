237b8ab99718d8dacc142ce342a0e7f3d919e29a
arbtin <arbtin@gmail.com>
2024-05-14 21:04:06 -0500

    initial commit

---
+++ src/test/java/com/arbtin/boatapp/Boats/BoatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbtin.boatapp.Boats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
